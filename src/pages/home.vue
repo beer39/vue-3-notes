33fd93c232c5f23a9374582cb382b7ea7f585647
medvedev_u <medvedev.yuriy@itmuse.ru>
2022-08-01 16:28:07 +0200

    add new functionaluty

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,28 @@
 <template>
-    <p>home</p>
+    <Form @onSubmit="handleSubmit" />
+    <List @onRemove="handleRemove" :items="notes"/>
 </template>
+
+<script>
+import Form from '@/components/Notes/Form.vue'
+import List from '@/components/Notes/List.vue'
+
+export default {
+    components: { Form, List },
+    data() {
+        return {
+            notes: ['task1', '24'],
+            tags: ['task1', '24'],
+        }
+    },
+    methods: {
+        handleSubmit(note) {
+            this.notes.push(note)
+            // console.log(note)
+        },
+        handleRemove(index) {
+            this.notes.splice(index, 1)
+        }
+    }
+}
+</script>
